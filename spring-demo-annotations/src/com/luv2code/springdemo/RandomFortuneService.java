9b5b051f7d2b89661b4983f90449272f2ce9a4a2
jedicode11 <92606781+jedicode11@users.noreply.github.com>
2021-10-18 17:28:52 +0300

    Add files via upload

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,28 @@
+package com.luv2code.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+		private String[] data = {
+				"Beware of the wolf in sheep's clothing",
+				"Diligance is the mother of good luck",
+				"The journey is the reward"
+		};
+		
+		private Random myRandom = new Random();
+		
+		@Override
+		public String getFortune() {
+			int index = myRandom.nextInt(data.length);
+			
+			String theFortune = data[index];
+			
+			return theFortune;
+		
+	}
+
+}
